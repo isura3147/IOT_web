fe3f4e1db865926047993e21c3b50d12a5752bed
Hashini Fernando <139649940+hashini-fernando@users.noreply.github.com>
2024-09-09 14:23:11 +0530

    commit this

--- CODE/website/iotwebsite/src/App.jsx
+++ CODE/website/iotwebsite/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import Navbar from './Components/Navbar/Navbar'
 import Background from './Components/Background/Background'
 import Services from './Components/Services/Services'
@@ -10,23 +11,24 @@ import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 
+
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Background/>
-      <div className="container">
+      
         <Title subTitle='Our Services' title='What we offer'/>
         <Services/>
         <About/>
-        <Title subTitle='Gallery' title='Projects done'/>
-        <Projects/>
+      
         <Title subTitle='TESTIMONIALS' title='What our clients say'/>
-        <Testimonials/>
+        <Testimonials/> {/* Keep this for Testimonials & Logos */}
         <Title subTitle='Contact us' title='Get in touch'/>
+
         <Contact/>
         <Footer/>
-      </div>
+    
     </div>
   )
 }
--- CODE/website/iotwebsite/src/Components/Background/Background.jsx
+++ CODE/website/iotwebsite/src/Components/Background/Background.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import './Background.css'
-import dark_arrow from '../../assets/dark-arrow.png'
+import React from 'react';
+import './Background.css';
+import dark_arrow from '../../assets/dark-arrow.png';
+
 const Background = () => {
   return (
-    <div className='background container'>
+    <div className='background background-container'>
       <div className='background-text'>
         <h1>Pioneering IoT to Reshape Global Interactions</h1>
-        <p>We have developed top-notch IOT devices and assited numerous
-          organizations in achieving economoic success.
+        <p>We have developed top-notch IOT devices and assisted numerous
+          organizations in achieving economic success.
         </p>
-        <button className='btn'>Contact us<img src={dark_arrow} alt='' /></button>
+        <button className='btn custom-btn'>Contact us<img src={dark_arrow} alt='' /></button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Background
+export default Background;
