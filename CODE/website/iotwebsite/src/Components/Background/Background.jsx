7e6f0db91f28cf0786bc2e24b7f980f95ad7c360
thirunilw <thirunilw@gmail.com>
2024-09-12 12:08:12 +0530

    background and services

--- CODE/website/iotwebsite/src/Components/Background/Background.jsx
+++ CODE/website/iotwebsite/src/Components/Background/Background.jsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-// import './Background.css'
-// import dark_arrow from '../../assets/dark-arrow.png'
-// const Background = () => {
-//   return (
-//     <div className='background container'>
-//       <div className='background-text'>
-//         <h1>Pioneering IoT to Reshape Global Interactions</h1>
-//         <p>We have developed top-notch IOT devices and assited numerous
-//           organizations in achieving economoic success.
-//         </p>
-//         <button className='btn'>Contact us<img src={dark_arrow} alt='' /></button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Background
-
 
 
 import React, { useState, useEffect } from 'react';
